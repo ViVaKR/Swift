cd0bc1d6bd315992fac32f366695f24d788d3b46
Kim Bum Jun <admin@vivabj.com>
2025-01-04 22:28:26 +0900

    Create new VivLogin

---
+++ VivLogin/VivLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VivLogin
+//
+//  Created by 김범준 on 1/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
